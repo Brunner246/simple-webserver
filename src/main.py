5ee5f5183694594c2326eecaddd2f757bd7affa1
MichaelBrunner <brunner@cadwork.swiss>
2024-03-14 18:53:57 +0100

    clean up

--- src/main.py
+++ src/main.py
@@ -2,62 +2,58 @@ from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
-
 db = client['exampleDB']
-
-app = Flask(__name__)
-
-my_data = "Hello, World!"
-
 mydb = db["mydatabase"]
 records = mydb["exampleCollection"]
 
 
-@app.route('/api', methods=['GET'])
-def get_data():
-    data = list(records.find({}, {'_id': False}))
-    return jsonify(data)
+class Cache:
+    def __init__(self):
+        self.data = {}
+
+    def add(self, key, value):
+        self.data[key] = value
+
+    def get(self, key):
+        return self.data.get(key)
+
+    def remove(self, key):
+        if key in self.data:
+            del self.data[key]
 
 
-@app.route('/api', methods=['POST'])
-def post_data():
-    data = request.get_json()
-    print(data)
-    if data:
-        key = "1234556"
-        value = data
-        records.update_one({key: {"$exists": True}}, {"$set": {key: value}}, upsert=True)
-    return jsonify({"received": data}), 200
+class MyApp:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.cache = Cache()
+        self.setup_routes()
 
+    def setup_routes(self):
+        @self.app.route('/api', methods=['POST'])
+        def post_data():
+            data = request.get_json()
+            print(data)
+            if data:
+                key = "1234556"
+                value = data
+                records.update_one({key: {"$exists": True}}, {"$set": {key: value}}, upsert=True)
+                self.cache.add(key, value)
+            return jsonify({"received": data}), 200
 
-@app.route('/')
-def index():
-    return """
-    <html>
-        <head>
-            <title>My Page</title>
-            <style>
-                body {
-                    background-color: #f0f0f0;
-                    font-family: Arial, sans-serif;
-                    margin: 0;
-                    padding: 0;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    height: 100vh;
-                }
-                h1 {
-                    color: #333;
-                }
-            </style>
-        </head>
-        <body>
-            <h1>Hello, World!</h1>
-        </body>
-    </html>
-    """
+        @self.app.route('/api', methods=['GET'])
+        def get_data():
+            data = list(records.find({}, {'_id': False}))
+            return jsonify(data)
+
+        @self.app.route('/', methods=['GET'])
+        def index():
+            with open('index.html') as f:
+                return f.read()
+
+    def run(self):
+        self.app.run(debug=True)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    my_app = MyApp()
+    my_app.run()
